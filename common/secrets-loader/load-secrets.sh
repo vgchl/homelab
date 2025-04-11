de9205a807cef15a087ca70f2ecd8ff6e23b243e
vgchl <1035578+vgchl@users.noreply.github.com>
2025-04-11 22:30:42 +0800

    Split up tang

--- common/secrets-loader/load-secrets.sh
+++ common/secrets-loader/load-secrets.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-set -e
+set -euo pipefail
 
 baseopts="list=echo,store=echo,delete=echo"
 
 for file in $SECRETS_DIR/*.jwe; do
     name=$(basename $file ".jwe")
     opts="${baseopts},lookup=clevis decrypt < $file"
-    podman secret create --replace --driver=shell --driver-opts="${opts}" "$name" "$file"
+    podman secret rm "$name" || true
+    podman secret create --driver=shell --driver-opts="${opts}" "$name" "$file"
 done
